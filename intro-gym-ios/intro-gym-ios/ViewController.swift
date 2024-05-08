6eaa1cae17fd493ecb6d53998931cb5df3d2e1b3
maestrying <mdwork96@gmail.com>
2024-05-08 12:51:09 +1000

    Initial commit

---
+++ intro-gym-ios/intro-gym-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  intro-gym-ios
+//
+//  Created by Дывак Максим on 08.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
