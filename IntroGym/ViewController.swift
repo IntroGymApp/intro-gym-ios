f873eceac131b6280094d16139c2971f0f48c916
maestrying <mdwork96@gmail.com>
2025-03-13 13:01:34 +1000

    initial commit

---
+++ IntroGym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroGym
+//
+//  Created by Дывак Максим on 12.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
